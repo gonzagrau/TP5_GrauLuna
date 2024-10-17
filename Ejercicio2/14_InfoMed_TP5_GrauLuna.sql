15d6e0ca214d0aefbd8c2dc9f2a286553f265812
agustinLunaSimondi <110484583+agustinLunaSimondi@users.noreply.github.com>
2024-10-17 16:28:45 -0300

    EJERCICIO 2.14

--- Ejercicio2/14_InfoMed_TP5_GrauLuna.sql
+++ Ejercicio2/14_InfoMed_TP5_GrauLuna.sql
@@ -1,3 +1,6 @@
+/* 14. Obtener el nombre del medicamento más recetado junto con la cantidad de recetas
+emitidas para ese medicamento.*/
+
 CREATE TABLE ultima_consulta AS
 SELECT p.nombre, MAX(c.fecha) AS ultima_consulta, c.diagnostico
 FROM pacientes p
